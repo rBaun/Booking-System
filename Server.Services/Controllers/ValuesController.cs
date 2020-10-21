07803dbe27a96514f96b3b71447b6e61ae70e8d6
rBaun <1067514@ucn.dk>
2020-10-21 14:13:44 +0200

    Added Exposed Services

--- Server.Services/Controllers/ValuesController.cs
+++ Server.Services/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Client.Services.Controllers
+namespace Server.Services.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
